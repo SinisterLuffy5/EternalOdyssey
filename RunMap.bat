d77081c14f0ee683ebde99d5b527fe46bc05e7b6
Gosick <vhd1180@hawkmail.hacc.edu>
2024-09-16 11:18:27 -0400

    Added a -speed command to test suite

---
+++ RunMap.bat
@@ -0,0 +1,5 @@
+@echo off
+
+cd C:\Users\phung\Desktop\Lab\Academic_Records_files\Desktop\Yeah\Old\Tools\UjAPI\UjAPI 241\
+
+UjAPILauncher.exe -loadfile C:\Users\phung\Desktop\Lab\Academic_Records_files\Desktop\EternalOdyssey\_build\EO.w3x
